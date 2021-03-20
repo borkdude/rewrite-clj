5537edb83b4813408c5efdb9392e35db265aaa61
Michiel Borkent <michielborkent@gmail.com>
2021-03-20 19:39:50 +0100

    bb.edn

--- script/apply_import_vars.clj
+++ script/apply_import_vars.clj
@@ -1,14 +1,11 @@
 #!/usr/bin/env bb
 
 (ns apply-import-vars
-  (:require [babashka.classpath :as cp]))
+  (:require [helper.env :as env]
+            [helper.shell :as shell]
+            [helper.status :as status]))
 
-(cp/add-classpath "./script")
-(require '[helper.env :as env]
-         '[helper.shell :as shell]
-         '[helper.status :as status])
-
-(defn main[args]
+(defn -main [& args]
   (env/assert-min-versions)
   (let [cmd (first args)]
     (when (not (#{"gen-code" "check"} cmd))
@@ -16,5 +13,3 @@
     (status/line :info (str "Running apply import vars " cmd))
     (shell/command ["clojure" "-X:apply-import-vars:script" cmd])
     nil))
-
-(main *command-line-args*)
